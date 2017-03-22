cf84c700160a4ce5ea16aebaf66a53b71111a7fd
Alina Jasniewski <alina.jasniewski@stonybrook.edu>
2017-03-22 13:27:13 -0400

    Merge manually config.php

--- api/Configuration/config.php
+++ api/Configuration/config.php
@@ -1,16 +1,26 @@
 <?php 
 
+//session_start();
+
+$cancerType = $_SESSION["cancerType"]; 
+//echo $cancerType;
+
 ///new
+//$baseUrl = "http://localhost:9099";
+//$serviceUrl = "$baseUrl/services/" . $cancerType;
 $baseUrl = "http://127.0.0.1:9099";
 $serviceUrl = "$baseUrl/services/Camicroscope3-QA";
+//$templateUrl = "$baseUrl/services/caMicroscope_Templates";
 
 $imageUrl = "$serviceUrl/ImageMetaData";
 
 $templateUrl = "$serviceUrl/AnnotationTemplate";
+
 $markupUrl = "$serviceUrl/Annotations";
 
 $dynamicServices = $serviceUrl;
 $firebase = "https://test-8f679.firebaseio.com/camicroscopeStates";
+$analysisMetaDataUrl = "$serviceUrl/Analyses_MetaData";
 
 
 $tempMarkupUrl = "http://localhost:9099/services/TCGABRCA_Dev";
@@ -21,17 +31,32 @@ return array(
      * temp
      */
     'algorithmsForImage' => "$serviceUrl/Provenance/query/getAlgorithmsForImage?",
-    'getMultipleAnnotations' => "$serviceUrl/Markups/query/getMultipleMarkups?",
+    'getMultipleAnnotations' => "$markupUrl/query/getMultipleMarkupsWithAttr?",
+    
+    //'getMultipleAnnotations' => "$serviceUrl/Markups/query/getMultipleMarkups?",
     //'getMultipleAnnotations' => "http://172.17.0.2:9099/services/Camicroscope_Annotations/MarkupLoader/query/getMultipleMarkups?",
 
 
     'firebase' => $firebase,
     'firebase_key' => $firebase_key,
     'retrieveTemplate' => "$serviceUrl/AnnotationTemplate/query/retrieveTemplate",
+    
+    
+    
+    
+    //'algorithmsForImage' => "$analysisMetaDataUrl/query/AlgorithmsForIID?",
+    'deleteAnnotation' => "$markupUrl/delete/DeleteByOID",
+    'postAlgorithmForImage' => "$analysisMetaDataUrl/submit/json?",
+    //'getMultipleAnnotations' => "$markupUrl/query/getMultipleMarkupsWithAttr?",
+    'getPropertiesForMarkup' => "$markupUrl/query/getPropertiesForMarkup?",
+    //'getFileLocation' => "$imageUrl/query/getFileLocationForIID?api_key=",
+    //'getMPP' => "$imageUrl/query/getMPPForIID?api_key=",
+    //'retrieveTemplate' => "$templateUrl/AnnotationTemplate/query/retrieveTemplate",
     'getAllAnnotations' => "$tempMarkupUrl/Annotations/query/byUserAndImageID?iid=",
     'getAnnotationsSpatial' => "$serviceUrl/GeoJSONImageMetaData/query/getMarkups?",
     'getAnnotationSpatialFilter' => "$tempMarkupUrl/Annotations/query/allByFilter?iid=",
     'postAnnotation' => "$serviceUrl/Markups/submit/json",
+    //'postAnnotation' => "$serviceUrl/Annotations/submit/json",
     'retrieveAnnotation' => "$tempMarkupUrl/Annotations/query/byAnnotId?annotId=",
     'postJobParameters' => "$tempMarkupUrl/AnalysisJobs/submit/singleJob",
     'deleteAnnotation' => "$tempMarkupUrl/Annotations/delete/singleAnnotation?annotId=",
@@ -41,7 +66,7 @@ return array(
     'fastcgi_server' => "/fastcgi-bin/iipsrv.fcgi",
     'postWorkOrder' => "$dynamicServices/WorkOrders/submit/json"
 
+    'fastcgi_server' => "/fcgi-bin/iipsrv.fcgi"
 );
-
 
 ?>
